3262ddfe11237d27f668ce746b553c538bec3792
ChelseNovitasari <chelseanovitasari47@gmail.com>
2026-09-18 14:43:49 +0700

    kategori seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -21,7 +21,10 @@ class CategorySeeder extends Seeder
         ];
 
         foreach ($categories as $category) {
-            Category::create($category);
+            Category::firstOrCreate(
+                ['slug' => $category['slug']],
+                $category                 
+            );
         }
     }
 }
